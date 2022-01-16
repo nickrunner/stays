86b821fb1739ef23cee759e7ea9bc9d38f47c9af
nickrunner <nick.a.schrock@gmail.com>
2022-01-16 23:19:31 +0000

    removing service account keys

--- stays-platform/src/firebase/firebase.ts
+++ stays-platform/src/firebase/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { App, initializeApp } from "firebase-admin/app";
+import { App, applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore, CollectionReference, DocumentData } from "firebase-admin/firestore";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,7 +10,7 @@ import { getFirestore, CollectionReference, DocumentData } from "firebase-admin/
 
 // Initialize Firebase
 
-const serviceAccount = require("C:/git/stays/stays-platform/stays-platform-firebase-adminsdk-lqrl0-17f55baaf8.json");
+const serviceAccount = require("C:/git/stays/stays-platform/stays-platform-firebase-adminsdk-lqrl0-e56f5dcabc.json");
 const admin = require('firebase-admin');
 export const app: App = initializeApp({
     credential: admin.credential.cert(serviceAccount)
