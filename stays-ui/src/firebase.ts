b80c3321da0f6eb27af864da2aa44c0faac54963
nickrunner <nick.a.schrock@gmail.com>
2022-02-10 04:35:04 +0000

    move comments

--- stays-ui/src/firebase.ts
+++ stays-ui/src/firebase.ts
@@ -3,8 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import {default as firebaseConfig} from "./secrets/stays_platform_creds.json";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
