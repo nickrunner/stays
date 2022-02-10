f4cffbc53be7bd4fd04d0db509f626bfc54aecf4
nickrunner <nick.a.schrock@gmail.com>
2022-02-10 04:23:12 +0000

    moving secrets

--- stays-ui/src/firebase.ts
+++ stays-ui/src/firebase.ts
@@ -2,21 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import {default as firebase} from "../stays_platform_creds.json";
+import {default as firebaseConfig} from "./secrets/stays_platform_creds.json";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCOTx6Bq-gIQDdfPDcxMGtZ0EC0FPGUTqg",
-  authDomain: "stays-platform.firebaseapp.com",
-  projectId: "stays-platform",
-  storageBucket: "stays-platform.appspot.com",
-  messagingSenderId: "705294162386",
-  appId: "1:705294162386:web:a2c429f8ecc866a284500b",
-  measurementId: "G-KH2T7DPT0Q"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
